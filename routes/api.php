11638d272cfb0fe3b94ba9658d39324b06dab3f8
Matt <mattsandovalb@gmail.com>
2023-02-22 15:44:23 +0100

    categroy tables

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,8 @@ use App\Models\Advertiser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
+
 
 Route::apiResource('jobs', JobsController::class);
 Route::apiResource('advertisers', AdvertiserController::class);
