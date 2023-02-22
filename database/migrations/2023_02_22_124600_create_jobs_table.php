e347128da6424d0e6c507793d5c2a54f4881974c
Albert Arques <albertarques@gmail.com>
2023-02-22 16:01:03 +0100

    JobsApi migration fields

--- database/migrations/2023_02_22_124600_create_jobs_table.php
+++ database/migrations/2023_02_22_124600_create_jobs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('picture');
             $table->string('description');
             $table->string('location');
             $table->string('salary');
